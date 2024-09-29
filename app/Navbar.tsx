5144dba6bd1f10ec0371d669ba09cb7126d7350a
Sajjan Karna <karnaa787@gmail.com>
2024-09-29 22:51:09 +0545

    feat: add navbar and shadcn

---
+++ app/Navbar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
+
+interface Link {
+  titile: string;
+  url: string;
+}
+
+const links: Link[] = [
+  { titile: "Home", url: "/" },
+  { titile: "Issues", url: "/issues" },
+];
+
+const Navbar = () => {
+  const currentPath = usePathname();
+
+  return (
+    <div className="flex items-center gap-x-5 bg-gray-200 p-5">
+      <div className="flex items-center gap-2 justify-center">
+        <AiFillBug size={30} color="#B8001F" />
+        <span className="font-bold">
+          Issue<span className="bg-yellow-500 px-1 rounded-sm">Hub</span>
+        </span>
+      </div>
+      <div>
+        {links.map((link) => (
+          <Link
+            key={link.url}
+            href={link.url}
+            className={classnames({
+              "mx-2 font-medium text-zinc-600 transition-colors hover:text-zinc-900":
+                true,
+              "text-zinc-900": link.url === currentPath,
+            })}
+          >
+            {link.titile}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Navbar from "./Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}`}>{children}</body>
+      <body className={`${poppins.className}`}>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
