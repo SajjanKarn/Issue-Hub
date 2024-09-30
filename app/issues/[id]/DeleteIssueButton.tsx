9713ad5d409d728a47676874360be0ff62f07468
Sajjan Karna <karnaa787@gmail.com>
2024-09-30 17:36:18 +0545

    feat: add alert dialog box while deleting issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,15 +2,43 @@ import { Button } from "@/components/ui/button";
 import { Issue } from "@prisma/client";
 import Link from "next/link";
 import { AiFillDelete } from "react-icons/ai";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const DeleteIssueButton = ({ issue }: { issue: Issue }) => {
   return (
-    <Link href={`/issues/${issue.id}/edit`}>
-      <Button className="flex w-[100%] gap-x-1" variant="destructive">
-        <AiFillDelete />
-        Delete
-      </Button>
-    </Link>
+    <>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button className="flex gap-x-1" variant="destructive">
+            <AiFillDelete />
+            Delete
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete the
+              issue <strong className="text-red-500">{issue.title}</strong>.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Delete</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
