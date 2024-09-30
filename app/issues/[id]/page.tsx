e5a5688aeeea1704070d9dcc26ae3235a0a1867e
Sajjan Karna <karnaa787@gmail.com>
2024-09-30 15:25:46 +0545

    feat: add edit issue button at /issue/[id]

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Card } from "@/components/ui/card";
 import prisma from "@/prisma/client";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { AiFillEdit } from "react-icons/ai";
 import ReactMarkdown from "react-markdown";
 
 interface IssueDetailProp {
@@ -24,20 +25,32 @@ const IssueDetail = async ({ params: { id } }: IssueDetailProp) => {
       <Link href="/issues">
         <Button variant="destructive">Go Back</Button>
       </Link>
-      <div className="mt-5">
-        <h1 className="text-4xl font-bold">{issue.title}</h1>
-        <div className="flex gap-x-5 my-2">
-          <IssueStatusBadge status={issue.status} />
-          <span className="text-zinc-600">
-            {issue.createdAt.toDateString()}
-          </span>
-        </div>
-      </div>
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+        <div>
+          <div className="mt-5">
+            <h1 className="text-4xl font-bold">{issue.title}</h1>
+            <div className="flex gap-x-5 my-2">
+              <IssueStatusBadge status={issue.status} />
+              <span className="text-zinc-600">
+                {issue.createdAt.toDateString()}
+              </span>
+            </div>
+          </div>
 
-      <div className="mt-3 prose">
-        <Card className="prose p-5">
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+          <div className="mt-3 prose">
+            <Card className="prose p-5">
+              <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
+          </div>
+        </div>
+        <div>
+          <Link href={`/issues/${issue.id}/edit`}>
+            <Button className="flex gap-x-1">
+              <AiFillEdit />
+              Edit
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
