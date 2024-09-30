ee8ed4d75db5eeb12649cb060b78342f2a96c2eb
Sajjan Karna <karnaa787@gmail.com>
2024-09-30 17:53:20 +0545

    fix: handle error while deleting issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -14,10 +14,25 @@ import { Button } from "@/components/ui/button";
 import { Issue } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { AiFillDelete } from "react-icons/ai";
 
 const DeleteIssueButton = ({ issue }: { issue: Issue }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      const response = await axios.delete(`/api/issue/${issue.id}`);
+      if (response.status === 200) {
+        router.push("/issues");
+        router.refresh();
+      }
+      // eslint-disable-next-line
+    } catch (error) {
+      setError(true);
+    }
+  };
 
   return (
     <>
@@ -40,15 +55,28 @@ const DeleteIssueButton = ({ issue }: { issue: Issue }) => {
             <AlertDialogCancel>Cancel</AlertDialogCancel>
             <AlertDialogAction
               className="bg-red-600 hover:bg-red-700 transition-colors"
-              onClick={async () => {
-                const response = await axios.delete(`/api/issue/${issue.id}`);
-                if (response.status === 200) {
-                  router.push("/issues");
-                  router.refresh();
-                }
-              }}
+              onClick={deleteIssue}
             >
               Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <AlertDialog open={error}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Something went wrong!</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogDescription>
+            An error occurred while deleting the issue. Please try again later.
+          </AlertDialogDescription>
+          <AlertDialogFooter>
+            <AlertDialogAction
+              className="bg-red-600 hover:bg-red-700 transition-colors"
+              onClick={() => setError(false)}
+            >
+              Dismiss
             </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
