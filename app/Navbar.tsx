19282cb3480a4703513226cbc61712a5c86f45be
Sajjan Karna <karnaa787@gmail.com>
2024-09-30 22:37:15 +0545

    fix: change layout of navbar for signin and signout

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -20,30 +20,32 @@ const Navbar = () => {
   const currentPath = usePathname();
 
   return (
-    <div className="flex items-center gap-x-5 border shadow p-5">
-      <div className="flex items-center gap-2 justify-center">
-        <AiFillBug size={30} color="#B8001F" />
-        <span className="font-bold">
-          Issue
-          <span className="ml-[0.5px] bg-red-500 px-1 rounded-sm text-white">
-            Hub
+    <div className="flex items-center justify-between gap-x-5 border shadow p-5">
+      <div className="flex items-center flex-[2]">
+        <div className="flex items-center gap-2 justify-center">
+          <AiFillBug size={30} color="#B8001F" />
+          <span className="font-bold">
+            Issue
+            <span className="ml-[0.5px] bg-red-500 px-1 rounded-sm text-white">
+              Hub
+            </span>
           </span>
-        </span>
-      </div>
-      <div>
-        {links.map((link) => (
-          <Link
-            key={link.url}
-            href={link.url}
-            className={classnames({
-              "mx-2 font-medium text-zinc-600 transition-colors hover:text-zinc-900":
-                true,
-              "text-zinc-900": link.url === currentPath,
-            })}
-          >
-            {link.titile}
-          </Link>
-        ))}
+        </div>
+        <div>
+          {links.map((link) => (
+            <Link
+              key={link.url}
+              href={link.url}
+              className={classnames({
+                "mx-2 font-medium text-zinc-600 transition-colors hover:text-zinc-900":
+                  true,
+                "text-zinc-900": link.url === currentPath,
+              })}
+            >
+              {link.titile}
+            </Link>
+          ))}
+        </div>
       </div>
       <div>
         {status === "authenticated" && (
