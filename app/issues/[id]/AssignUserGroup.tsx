4e6d299d1e58cb9b39112dac817b86f15d6becb8
Sajjan Karna <karnaa787@gmail.com>
2024-10-01 14:54:27 +0545

    refactor: refactor AssignUserGroup

--- app/issues/[id]/AssignUserGroup.tsx
+++ app/issues/[id]/AssignUserGroup.tsx
@@ -13,40 +13,33 @@ import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 
 const AssignUserGroup = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/user").then((res) => res.data),
-    staleTime: 1000 * 60 * 5, // 5 minutes
-    retry: 3,
-  });
+  const { data: users, error, isLoading } = useUser();
 
   if (isLoading) return <Skeleton width="10rem" height="2rem" />;
 
   if (error) return null;
 
+  const handleAssignUser = async (userId: string) => {
+    try {
+      const result = await axios.patch(`/api/issue/${issue.id}`, {
+        assignedToUserId: userId === "null" ? null : userId,
+      });
+      if (result.status === 200 && userId === "null") {
+        toast.success("User unassigned successfully");
+        return;
+      }
+      if (result.status === 200) {
+        toast.success("User assigned successfully");
+      }
+      // eslint-disable-next-line
+    } catch (error) {
+      toast.error("Failed to assign user");
+    }
+  };
+
   return (
     <Select
-      onValueChange={async (userId) => {
-        try {
-          const result = await axios.patch(`/api/issue/${issue.id}`, {
-            assignedToUserId: userId === "null" ? null : userId,
-          });
-          if (result.status === 200 && userId === "null") {
-            toast.success("User unassigned successfully");
-            return;
-          }
-          if (result.status === 200) {
-            toast.success("User assigned successfully");
-          }
-          // eslint-disable-next-line
-        } catch (error) {
-          toast.error("Failed to assign user");
-        }
-      }}
+      onValueChange={handleAssignUser}
       defaultValue={issue.assignedToUserId || "null"}
     >
       <SelectTrigger className="w-full xl:w-[200px]">
@@ -65,5 +58,13 @@ const AssignUserGroup = ({ issue }: { issue: Issue }) => {
     </Select>
   );
 };
+
+const useUser = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/user").then((res) => res.data),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+    retry: 3,
+  });
 
 export default AssignUserGroup;
