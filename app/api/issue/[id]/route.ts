dcc0f00121a2068a1e2935f83545006749f325c6
Sajjan Karna <karnaa787@gmail.com>
2024-10-01 13:36:42 +0545

    fix: protect /api for edit and delete also made patch more flexible

--- app/api/issue/[id]/route.ts
+++ app/api/issue/[id]/route.ts
@@ -1,43 +1,81 @@
 import prisma from "@/prisma/client";
-import issueSchema from "@/schemas/validationSchema";
+import { patchIssueSchema } from "@/schemas/validationSchema";
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized." }, { status: 401 });
+  }
 
-  if (!validation.success) {
+  try {
+    const body = await request.json();
+    if (!body) {
+      return NextResponse.json({ error: "Invalid body." }, { status: 400 });
+    }
+
+    const validation = patchIssueSchema.safeParse(body);
+
+    if (!validation.success) {
+      return NextResponse.json(
+        { errors: validation.error.format() },
+        { status: 400 }
+      );
+    }
+
+    const { assignedToUserId, title, description } = body;
+    if (assignedToUserId) {
+      const user = await prisma.user.findUnique({
+        where: { id: assignedToUserId },
+      });
+      if (!user) {
+        return NextResponse.json(
+          { error: "Invalid user ID." },
+          { status: 400 }
+        );
+      }
+    }
+
+    const issue = await prisma.issue.findUnique({
+      where: { id: parseInt(params.id) },
+    });
+    if (!issue) {
+      return NextResponse.json({ error: "Invalid issue." }, { status: 404 });
+    }
+
+    const updatedIssue = await prisma.issue.update({
+      where: { id: parseInt(params.id) },
+      data: {
+        title: title,
+        description: description,
+        assignedToUserId,
+      },
+    });
+
+    return NextResponse.json(updatedIssue, { status: 200 });
+    // eslint-disable-next-line
+  } catch (e) {
     return NextResponse.json(
-      { errors: validation.error.format() },
+      { error: "Something went wrong" },
       { status: 400 }
     );
   }
-
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
-  if (!issue) {
-    return NextResponse.json({ error: "Invalid issue." }, { status: 404 });
-  }
-
-  const updatedIssue = await prisma.issue.update({
-    where: { id: parseInt(params.id) },
-    data: {
-      title: body.title,
-      description: body.description,
-    },
-  });
-
-  return NextResponse.json(updatedIssue, { status: 200 });
 }
 
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized." }, { status: 401 });
+  }
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
--- schemas/validationSchema.ts
+++ schemas/validationSchema.ts
@@ -5,4 +5,25 @@ const issueSchema = z.object({
   description: z.string().min(1),
 });
 
+const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(5, "Title must be at least 5 characters long.")
+    .max(255, "Title must be at most 255 characters long.")
+    .optional(),
+  description: z
+    .string()
+    .min(1, "Description must be at least 1 character long.")
+    .max(65000, "Title must not be greater than 65000 characters")
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "Assigned to user ID must be at least 1 character long.")
+    .max(255)
+    .optional()
+    .nullable(),
+});
+
+export { patchIssueSchema };
+
 export default issueSchema;
