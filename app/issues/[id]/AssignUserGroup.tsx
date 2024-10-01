6aa032ebcc7fa3870297e53a2d4862da9bb3e8e1
Sajjan Karna <karnaa787@gmail.com>
2024-10-01 12:22:13 +0545

    feat: populate the user field in dropdown to assign user

--- app/issues/[id]/AssignUserGroup.tsx
+++ app/issues/[id]/AssignUserGroup.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Select,
   SelectContent,
@@ -5,15 +6,34 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { User } from "@prisma/client";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AssignUserGroup = () => {
+  const [user, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await axios.get("/api/user");
+      if (res.status === 200) {
+        setUsers(res.data);
+      }
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select>
       <SelectTrigger className="w-full xl:w-[200px]">
         <SelectValue placeholder="Assign a user..." />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="1">Sajjan Karna</SelectItem>
+        {user.map((user) => (
+          <SelectItem key={user.id} value={user.id}>
+            {user.name}
+          </SelectItem>
+        ))}
       </SelectContent>
     </Select>
   );
